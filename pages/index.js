1e01bd0bf69be1b4c4f5f94ed0f75e67574a4e89
Prince <pondonda@gmail.com>
2021-08-24 10:17:26 +0200

    adding twitter card

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,17 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Swootte - Accept digital dollar for your physical commerce</title>
         <link rel="icon" href="/favicon.ico" />
+        
+        <meta property="og:title" content="Swootte - Accept digital dollar for your physical commerce" />
+        <meta property="og:url" content="https://www.swootte.com/" />
+        <meta property="og:description" content="Swootte Terminal allows you to create your point of sales solution to accept digital dollar payments on the spot. Designed for modern merchants and platforms, our terminals helps you enter the new internet financial world." />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1508938255445-041651dfe0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80" />
+        <meta name="twitter:card" content="summary"></meta>
+
+        <meta name="twitter:site" content="@swootte" />
+        <meta name="twitter:creator" content="@ondpr" />
       </Head>
 
       <Nav></Nav>

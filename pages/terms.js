d15661898d473b802f04711165e9855d0e9ecd6c
Prince <pondonda@gmail.com>
2021-11-15 11:00:26 +0100

    adding terms page

--- pages/terms.js
+++ pages/terms.js
@@ -36,10 +36,185 @@ const Terms = () => {
                     Please read the terms carefully as they govern your use of Swootte Services.
                 </div>
 
-                <div className="px-16 mt-5 font-semibold">I. Definitions</div>
-                <div className="px-16 mt-5">
+                <div className="flex-1 min-w-0 px-20">
 
-                </div>
+                
+                <div className="py-5">
+                <h2 className="text-lg font-bold py-10 text-gray-600">1. Présentation </h2>
+                  <h2 className="text-base font-bold text-gray-500">En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet www.getkookers.com l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi: </h2>
+                  <p className="py-3"> Swootte – 71-75 Shelton St, London WC2H 9JQ
+                      Responsable publication : ONDONDA Prince – prince.ondonda@swotte.com
+                      Le responsable publication est une personne physique ou une personne morale.
+                      Webmaster : Swootte.com
+                      Hébergeur : 1and1 – 7 Place de la Gare 57200 Sarreguemines 0970 808 911
+                      Délégué à la protection des données : ONDONDA Prince – prince.ondonda@swotte.com</p>
+
+                  <p className="py-3">Swootte est une entreprise de service financiers basé à londres, elle fournie des application web et mobile pouvant etre utilisé par tout le monde. </p>
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">2. Nature des données collectées </h2>
+
+                  <p>Dans le cadre de l'utilisation de Swootte, Tinda , Nous sommes susceptible de collecter les catégories de données suivantes concernant ses Utilisateurs : Données d'état-civil, d'identité, d'identification...Données de connexion (adresses IP, journaux d'événements...) Données de localisation (déplacements, données GPS, GSM...).</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">3. Communication des données personnelles à des tiers </h2>
+
+                  <p>Pas de communication à des tiers.
+      Vos données ne font l'objet d'aucune communication à des tiers. Vous êtes toutefois informés qu'elles pourront être divulguées en application d'une loi, d'un règlement ou en vertu d'une décision d'une autorité réglementaire ou judiciaire compétente.</p>
+
+                  
+              </div>
+
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">4. Information préalable pour la communication des données personnelles à des tiers en cas de fusion / absorption </h2>
+
+                  <p>Collecte de l’opt-in (consentement) préalable à la transmission des données suite à une fusion / acquisition
+Dans le cas où nous prendrions part à une opération de fusion, d’acquisition ou à toute autre forme de cession d’actifs, nous nous engageons à obtenir votre consentement préalable à la transmission de vos données personnelles et à maintenir le niveau de confidentialité de vos données personnelles auquel vous avez consenti.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">5. Agrégation des données </h2>
+
+                  <p>Agrégation avec des données non personnelles
+Nous pouvons publier, divulguer et utiliser les informations agrégées (informations relatives à tous nos Utilisateurs ou à des groupes ou catégories spécifiques d'Utilisateurs que nous combinons de manière à ce qu'un Utilisateur individuel ne puisse plus être identifié ou mentionné) et les informations non personnelles à des fins d'analyse du secteur et du marché, de profilage démographique, à des fins promotionnelles et publicitaires et à d'autres fins commerciales.
+
+Agrégation avec des données personnelles disponibles sur les comptes sociaux de l'Utilisateur
+Si vous connectez votre compte à un compte d’un autre service afin de faire des envois croisés, ledit service pourra nous communiquer vos informations de profil, de connexion, ainsi que toute autre information dont vous avez autorisé la divulgation. Nous pouvons agréger les informations relatives à tous nos autres Utilisateurs, groupes, comptes, aux données personnelles disponibles sur l’Utilisateur.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">6. Collecte des données d'identité </h2>
+
+                  <p>Inscription et identification préalable pour la fourniture du service
+L’utilisation du Site nécessite une inscription et une identification préalable. Vos données nominatives (nom, prénom, adresse postale, e-mail, numéro de téléphone,...) sont utilisées pour exécuter nos obligations légales résultant de la livraison des produits et / ou des services, en vertu du Contrat de licence utilisateur final, de la Limite de garantie, le cas échéant, ou de toute autre condition applicable. Vous ne fournirez pas de fausses informations nominatives et ne créerez pas de compte pour une autre personne sans son autorisation. Vos coordonnées devront toujours être exactes et à jour.</p>
+
+                  
+              </div>
+
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">7. Collecte des données d'identification </h2>
+
+                  <p>Utilisation de l'identifiant de l’utilisateur uniquement pour l’accès aux services
+Nous utilisons vos identifiants électroniques seulement pour et pendant l'exécution du contrat.</p>
+
+                  
+              </div>
+
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">8. Géolocalisation </h2>
+
+                  <p>Géolocalisation à des fins de fourniture du service
+Nous collectons et traitons vos données de géolocalisation afin de vous fournir nos services. Nous pouvons être amenés à faire usage des données personnelles dans le but de déterminer votre position géographique en temps réel. Conformément à votre droit d'opposition prévu par la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous avez la possibilité, à tout moment, de désactiver les fonctions relatives à la géolocalisation.
+
+Géolocalisation à des fins de croisement
+Nous collectons et traitons vos données de géolocalisation afin de permettre à nos services d'identifier les points de croisement dans le temps et dans l'espace avec d'autres Utilisateurs du service afin de vous présenter le profil des Utilisateurs croisés. Conformément à votre droit d'opposition prévu par la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous avez la possibilité, à tout moment, de désactiver les fonctions relatives à la géolocalisation. Vous reconnaissez alors que le service ne sera plus en mesure de vous présenter de profil des autres Utilisateurs.
+
+Géolocalisation avec mise à disposition des partenaires pour référencement et agrégation (avec opt-in)
+Nous pouvons collecter et traiter vos données de géolocalisation avec nos partenaires. Nous nous engageons à anonymiser les données utilisées. Conformément à votre droit d'opposition prévu par la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous avez la possibilité, à tout moment, de désactiver les fonctions relatives à la géolocalisation.</p>
+
+                  
+              </div>
+
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">9. Collecte des données du terminal</h2>
+
+                  <p>Collecte des données de profilage et des données techniques à des fins de fourniture du service
+Certaines des données techniques de votre appareil sont collectées automatiquement par le Site. Ces informations incluent notamment votre adresse IP, fournisseur d'accès à Internet, configuration matérielle, configuration logicielle, type et langue du navigateur... La collecte de ces données est nécessaire à la fourniture des services.
+
+Collecte des données techniques à des fins publicitaires, commerciales et statistiques
+Les données techniques de votre appareil sont automatiquement collectées et enregistrées par le Site, à des fins publicitaires, commerciales et statistiques. Ces informations nous aident à personnaliser et à améliorer continuellement votre expérience sur notre Site. Nous ne collectons ni ne conservons aucune donnée nominative (nom, prénom, adresse...) éventuellement attachée à une donnée technique. Les données collectées sont susceptibles d’être revendues à des tiers.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">10. Délai de conservation des données personnelles et d'anonymisation </h2>
+
+                  <p>Conservation des données pendant la durée de la relation contractuelle
+Conformément à l'article 6-5° de la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, les données à caractère personnel faisant l'objet d'un traitement ne sont pas conservées au-delà du temps nécessaire à l'exécution des obligations définies lors de la conclusion du contrat ou de la durée prédéfinie de la relation contractuelle.
+
+Conservation des données anonymisées au delà de la relation contractuelle / après la suppression du compte
+Nous conservons les données personnelles pour la durée strictement nécessaire à la réalisation des finalités décrites dans les présentes CGU. Au-delà de cette durée, elles seront anonymisées et conservées à des fins exclusivement statistiques et ne donneront lieu à aucune exploitation, de quelque nature que ce soit.
+
+Suppression des données après suppression du compte
+Des moyens de purge de données sont mis en place afin d'en prévoir la suppression effective dès lors que la durée de conservation ou d'archivage nécessaire à l'accomplissement des finalités déterminées ou imposées est atteinte. Conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez par ailleurs d'un droit de suppression sur vos données que vous pouvez exercer à tout moment en prenant contact avec l'Éditeur.
+
+Suppression des données après 3 ans d'inactivité
+Pour des raisons de sécurité, si vous ne vous êtes pas authentifié sur le Site pendant une période de trois ans, vous recevrez un e-mail vous invitant à vous connecter dans les plus brefs délais, sans quoi vos données seront supprimées de nos bases de données.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">11. Suppression du compte</h2>
+
+                  <p>Suppression du compte à la demande
+L'Utilisateur a la possibilité de supprimer son Compte à tout moment, par simple demande à l'Éditeur OU par le menu de suppression de Compte présent dans les paramètres du Compte le cas échéant.
+
+Suppression du compte en cas de violation des CGU
+En cas de violation d'une ou de plusieurs dispositions des CGU ou de tout autre document incorporé aux présentes par référence, l'Éditeur se réserve le droit de mettre fin ou restreindre sans aucun avertissement préalable et à sa seule discrétion, votre usage et accès aux services, à votre compte et à tous les Sites.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">12. Indications en cas de faille de sécurité décelée par l'Éditeur </h2>
+
+                  <p>Information de l'Utilisateur en cas de faille de sécurité
+Nous nous engageons à mettre en oeuvre toutes les mesures techniques et organisationnelles appropriées afin de garantir un niveau de sécurité adapté au regard des risques d'accès accidentels, non autorisés ou illégaux, de divulgation, d'altération, de perte ou encore de destruction des données personnelles vous concernant. Dans l'éventualité où nous prendrions connaissance d'un accès illégal aux données personnelles vous concernant stockées sur nos serveurs ou ceux de nos prestataires, ou d'un accès non autorisé ayant pour conséquence la réalisation des risques identifiés ci-dessus, nous nous engageons à :
+
+Vous notifier l'incident dans les plus brefs délais ;
+Examiner les causes de l'incident et vous en informer ;
+Prendre les mesures nécessaires dans la limite du raisonnable afin d'amoindrir les effets négatifs et préjudices pouvant résulter dudit incident
+Limitation de la responsabilité
+En aucun cas les engagements définis au point ci-dessus relatifs à la notification en cas de faille de sécurité ne peuvent être assimilés à une quelconque reconnaissance de faute ou de responsabilité quant à la survenance de l'incident en question.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">13. Transfert des données personnelles à l'étranger </h2>
+
+                  <p>Pas de transfert en dehors de l'Union européenne
+L'Éditeur s'engage à ne pas transférer les données personnelles de ses Utilisateurs en dehors de l'Union européenne.</p>
+
+                  
+              </div>
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">14. Transfets </h2>
+
+                <p>Swootte vous fourni la sécurité la plus optimal pour proteger votre compte en s'aidant de divers outil tel que le code personnel. vous etes le seul à le connaitre, le seul à le definir , par conséquent toutes les transactions effecuté à votre nom à l'aide de nos services, applications ne pourront nous etre imputé, en acceptant ces termes, vous le reconnaissez. 
+                </p>
+
+                  {/* <p>Le traitement des transactions pour les clients ou les chefs sur kookers sont effectués par Stripe et sont soumis à l'acceptation des conditions de service et d'utilisation Stripe. En acceptant nos présentes conditions d'utilisation, vous acceptez aussi les conditions d'utilisation de Stripe. Comme condition pour que Swootte puisse offrir des services de traitement des paiements par l'intermédiaire de Stripe, vous acceptez de fournir à Swootte. </p> */}
+
+                  
+              </div>
+
+
+              <div className="py-5">
+                <h2 className="text-lg font-bold py-6 text-gray-600">15. Modification des CGU et de la politique de confidentialité </h2>
+
+                  <p>En cas de modification des présentes CGU, engagement de ne pas baisser le niveau de confidentialité de manière substantielle sans l'information préalable des personnes concernées
+Nous nous engageons à vous informer en cas de modification substantielle des présentes CGU, et à ne pas baisser le niveau de confidentialité de vos données de manière substantielle sans vous en informer et obtenir votre consentement.</p>
+
+                  
+              </div>
+              </div>
+                
 
 
                 

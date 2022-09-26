a7961c0e1fa25e4194eff382b22d549b73c8062d
prince ONDONDA <princeondonda@MacBook-Pro.local>
2022-09-26 15:56:54 +0200

    arrange this

--- components/nav/nav_alone.js
+++ components/nav/nav_alone.js
@@ -1,5 +1,5 @@
 import styles from "../../styles/Home.module.css"
-import { SwootteLogo } from "../dashboard/nav";
+import SwootteLogo from "../logo/swootte";
 import Link from "next/dist/client/link";
 
 
@@ -8,7 +8,6 @@ export const NavAlone = () => {
     return (
         <div style={styles} className="bg-white w-full h-16 border-b border-gray-200 top-0 sticky">
             <nav>
-
                 <div className="px-4 py-5  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                     <div className="relative flex items-center justify-between">
                         <div className="flex items-center">
